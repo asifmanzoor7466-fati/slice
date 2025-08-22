02189d95bc9f67076fe83a5a01bcabc9a88fca15
Asif Manzoor <asifmanzoor7466@gmail.com>
2025-08-22 12:48:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
 
-    function checkNumber() {
-      const input = document.getElementById("numInput").value;
-      const result = document.getElementById("result");
+ function checkNumber() {
+  const input = document.getElementById("numInput").value;
+  const result = document.getElementById("result");
 
-      try {
-        if (input === "") {
-          throw "Input is empty!";
-        }
-        if (isNaN(input)) {
-          throw "Not a number!";
-        }
-        if (Number(input) < 0) {
-          throw "Number cannot be negative!";
-        }
-
-        result.innerHTML = "You entered: " + input;
-      } catch (err) {
-        result.innerHTML = "Error: " + err;
-      }
+  try {
+    if (input === "") {
+      throw "Input is empty!";
     }
+
+    const num = Number(input);  
+
+    if (isNaN(num)) {
+      throw "Not a number!";
+    }
+    if (num < 0) {
+      throw "Number cannot be negative!";
+    }
+
+    result.innerHTML = "You entered: " + num;
+  } catch (err) {
+    result.innerHTML = "Error: " + err;
+  }
+}
 
 
 
